36f7706359d03f36833f5b3527e67d38d7490aaf
Muhammet Furkan DOĞAN <dogan.furkan@msn.com>
2018-03-26 13:49:17 +0300

    IMDBGraphsearcher yapıldı.
    imdb sitesi için film infolarını çeke graperyapıldı. test methodu yazıldı.

---
+++ src/Graphlax/IMDB/Film.cs
@@ -0,0 +1,12 @@
+namespace Graphlax.IMDB
+{
+    public class Film
+    {
+        public string Name { get; set; }
+        public string Duraction { get; set; }
+        public string AVGRating { get; set; }
+        public string MaxRating { get; set; }
+        public string TotalRatingCount { get; set; }
+        public string VisionYear { get; set; }
+    }
+}
--- src/Graphlax/IMDB/IMDBGraphObject.cs
+++ src/Graphlax/IMDB/IMDBGraphObject.cs
@@ -3,6 +3,19 @@ namespace Graphlax.IMDB
     public class IMDBGraphObject
         :GraphObject
     {
-
+        public IMDBGraphObject()
+        {
+            
+        }
+        public IMDBGraphObject(GraphObject graphObject)
+        {
+            this.Description=graphObject.Description;
+            this.Image=graphObject.Image;
+            this.Site=graphObject.Site;
+            this.Title=graphObject.Title;
+            this.Type=graphObject.Type;
+            this.Url=graphObject.Url;
+        }
+        public Film Info { get; set; }
     }
 }
