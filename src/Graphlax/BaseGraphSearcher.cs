c8bcf228c6d5ddb039dad12f84b4b5d5a035e6ca
Muhammet Furkan DOĞAN <dogan.furkan@msn.com>
2018-03-26 13:48:31 +0300

    defautlgrapsearcher basegrapsearcher a taşındı.

--- src/Graphlax/BaseGraphSearcher.cs
+++ src/Graphlax/BaseGraphSearcher.cs
@@ -1,12 +1,14 @@
 using System;
+using Graphlax.Helpers;
 using HtmlAgilityPack;
 
 namespace Graphlax
 {
     public abstract class BaseGraphSearcher<TGraph>
         :IGraphSearcher<TGraph>
-        where TGraph:GraphObject
+        where TGraph:GraphObject, new()
     {
+        protected HtmlDocument htmlDocument;
         public BaseGraphSearcher()
         {
 
@@ -18,6 +20,59 @@ namespace Graphlax
             return web.Load(uri);
         }
 
-        public abstract TGraph Search(Uri uri);
+        public virtual TGraph Search(Uri uri)
+        {
+            htmlDocument=GetHtmlDocument(uri);
+            if(htmlDocument==null)
+            {
+                throw new NullReferenceException(nameof(htmlDocument));
+            }
+            
+            HtmlNode titleNode = htmlDocument
+                .DocumentNode
+                .SelectSingleNode(@"/html/head/meta[@property='og:title']");
+            HtmlNode siteTitleNode=htmlDocument
+                .DocumentNode
+                .SelectSingleNode(@"/html/head/title");
+            HtmlNode descriptionNode = htmlDocument
+                .DocumentNode
+                .SelectSingleNode(@"/html/head/meta[@property='og:description']");
+            HtmlNode typeNode=htmlDocument
+                .DocumentNode
+                .SelectSingleNode(@"/html/head/meta[@property='og:type']");
+            HtmlNode imageSrcNode=htmlDocument
+                .DocumentNode
+                .SelectSingleNode(@"/html/head/meta[@property='og:image']");
+            HtmlNode imageTypeNode=htmlDocument
+                .DocumentNode
+                .SelectSingleNode(@"/html/head/meta[@property='og:image:type']");
+            HtmlNode imageHeightNode=htmlDocument
+                .DocumentNode
+                .SelectSingleNode(@"/html/head/meta[@property='og:image:height']");
+            HtmlNode imageWidthNode=htmlDocument
+                .DocumentNode
+                .SelectSingleNode(@"/html/head/meta[@property='og:image:width']");
+            HtmlNode imageAltNode=htmlDocument
+                .DocumentNode
+                .SelectSingleNode(@"/html/head/meta[@property='og:image:alt']");
+            
+            return new TGraph(){
+                Url=uri.ToString(),
+                Title=titleNode.GetAttributeValueWithNullCheck("content",string.Empty),
+                Type=typeNode.GetAttributeValueWithNullCheck("content",string.Empty).ToGraphType(),
+                Description=descriptionNode.GetAttributeValueWithNullCheck("content",string.Empty),
+                Image=new Graphlax.Image(){
+                    Heigth=imageHeightNode.GetAttributeValueWithNullCheck("content","0").ToInt(),
+                    Width=imageWidthNode.GetAttributeValueWithNullCheck("content","0").ToInt(),
+                    Url=imageSrcNode.GetAttributeValueWithNullCheck("content",string.Empty).ToString(),
+                    Alt=imageAltNode.GetAttributeValueWithNullCheck("content",string.Empty)
+                },
+                Site=new Graphlax.Site(){
+                    Title=siteTitleNode.InnerText.ToString(),
+                    Name=uri.Host.ToString(),
+                    IP="127.0.0.1"
+                }
+            };
+        }
     }
 }
